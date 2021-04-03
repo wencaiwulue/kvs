d5f8dac64124371cffb6ec412ab06cc7f43f5446
wencaiwulue <895703375@qq.com>
2021-04-03 19:14:12 +0800

    optimize struct

--- src/main/java/KvsApplication.java
+++ src/main/java/KvsApplication.java
@@ -1,17 +1,31 @@
+import com.google.common.base.Strings;
+import lombok.extern.slf4j.Slf4j;
 import raft.Node;
 import raft.NodeAddress;
-import rpc.model.requestresponse.Request;
-import rpc.model.requestresponse.Response;
 import rpc.netty.server.WebSocketServer;
 import util.ThreadUtil;
 
 import java.net.InetSocketAddress;
-import java.util.function.Function;
 
-
+@Slf4j
 public class KvsApplication {
+
     public static void main(String[] args) {
-        InetSocketAddress local = new InetSocketAddress("127.0.0.1", Integer.parseInt(System.getenv("port")));
+        String portStr = System.getenv("port");
+        if (Strings.isNullOrEmpty(portStr)) {
+            log.error("Not set port, needs to set it! exiting");
+            System.exit(-1);
+        }
+        int port = 0;
+        try {
+            port = Integer.parseInt(portStr);
+        } catch (NumberFormatException exception) {
+            log.error("Can not parse string: {} to int, exiting", portStr);
+            System.exit(-2);
+        }
+
+
+        InetSocketAddress local = new InetSocketAddress("127.0.0.1", port);
         Node node = Node.of(new NodeAddress(local));
 
         ThreadUtil.getThreadPool().execute(node::start);
